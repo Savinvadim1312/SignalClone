b17162cfd6227179b12cc5623d1454e697886260
Vadim Savin <savinvadim1312@gmail.com>
2021-08-20 18:28:19 +0100

    fetch chat rooms

--- components/ChatRoomItem/ChatRoomItem.tsx
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -1,32 +1,53 @@
-import React from 'react';
-import { Text, Image, View, Pressable } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, Image, View, Pressable, ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
+import { DataStore } from '@aws-amplify/datastore';
+import { ChatRoomUser, User } from '../../src/models';
 import styles from './styles';
+import Auth from '@aws-amplify/auth';
 
 export default function ChatRoomItem({ chatRoom }) {
-  const user = chatRoom.users[1];
+  // const [users, setUsers] = useState<User[]>([]); // all users in this chatroom
+  const [user, setUser] = useState<User|null>(null); // the display user
 
   const navigation = useNavigation();
+  console.log(chatRoom);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const fetchedUsers = (await DataStore.query(ChatRoomUser))
+        .filter(chatRoomUser => chatRoomUser.chatroom.id === chatRoom.id)
+        .map(chatRoomUser => chatRoomUser.user);
+
+      // setUsers(fetchedUsers);
+
+      const authUser = await Auth.currentAuthenticatedUser();
+      setUser(fetchedUsers.find(user => user.id !== authUser.attributes.sub) || null);
+    };
+    fetchUsers();
+  }, []);
 
   const onPress = () => {
-    console.warn('pressed on ', user.name)
     navigation.navigate('ChatRoom', { id: chatRoom.id });
+  }
+
+  if (!user) {
+    return <ActivityIndicator />
   }
 
   return (
     <Pressable onPress={onPress} style={styles.container}>
       <Image source={{ uri: user.imageUri}} style={styles.image} />
 
-      {chatRoom.newMessages && <View style={styles.badgeContainer}>
+      {!!chatRoom.newMessages && <View style={styles.badgeContainer}>
         <Text style={styles.badgeText}>{chatRoom.newMessages}</Text>
       </View>}
 
       <View style={styles.rightContainer}>
         <View style={styles.row}>
           <Text style={styles.name}>{user.name}</Text>
-          <Text style={styles.text}>{chatRoom.lastMessage.createdAt}</Text>
+          <Text style={styles.text}>{chatRoom.lastMessage?.createdAt}</Text>
         </View>
-        <Text numberOfLines={1} style={styles.text}>{chatRoom.lastMessage.content}</Text>
+        <Text numberOfLines={1} style={styles.text}>{chatRoom.lastMessage?.content}</Text>
       </View>
     </Pressable>
   );
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,26 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Text, Image, Pressable, View, StyleSheet, FlatList } from 'react-native';
-import { Auth } from 'aws-amplify';
+import { Auth, DataStore } from 'aws-amplify';
+import { ChatRoom, ChatRoomUser } from '../src/models';
 import ChatRoomItem from '../components/ChatRoomItem';
 
-import chatRoomsData from '../assets/dummy-data/ChatRooms';
 
 export default function TabOneScreen() {
+  const [chatRooms, setChatRooms] = useState<ChatRoom[]>([]);
+
+  useEffect(() => {
+    const fetchChatRooms = async () => {
+      const userData = await Auth.currentAuthenticatedUser();
+
+      const chatRooms = (await DataStore.query(ChatRoomUser))
+        .filter(chatRoomUser => chatRoomUser.user.id === userData.attributes.sub)
+        .map(chatRoomUser => chatRoomUser.chatroom);
+
+      setChatRooms(chatRooms);
+    };
+    fetchChatRooms();
+  }, []);
 
   const logOut = () => {
     Auth.signOut();
@@ -15,7 +29,7 @@ export default function TabOneScreen() {
   return (
     <View style={styles.page}>
        <FlatList 
-        data={chatRoomsData}
+        data={chatRooms}
         renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
         showsVerticalScrollIndicator={false}
       />
