3c34eea48c5404e7242a7a199907339afbbf81b2
Vadim Savin <savinvadim1312@gmail.com>
2021-08-27 15:51:48 +0100

    emojis

--- components/MessageInput/MessageInput.tsx
+++ components/MessageInput/MessageInput.tsx
@@ -1,44 +1,57 @@
-import React, { useState } from 'react';
-import { 
-  View, 
-  Text, 
-  StyleSheet, 
-  TextInput, 
-  Pressable, 
-  KeyboardAvoidingView, 
-  Platform 
-} from 'react-native';
-import { SimpleLineIcons, Feather, MaterialCommunityIcons, AntDesign, Ionicons } from '@expo/vector-icons'; 
-import { DataStore } from '@aws-amplify/datastore';
-import { ChatRoom, Message } from '../../src/models';
-import { Auth } from 'aws-amplify';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import {
+  SimpleLineIcons,
+  Feather,
+  MaterialCommunityIcons,
+  AntDesign,
+  Ionicons,
+} from "@expo/vector-icons";
+import { DataStore } from "@aws-amplify/datastore";
+import { ChatRoom, Message } from "../../src/models";
+import { Auth } from "aws-amplify";
+import EmojiSelector from "react-native-emoji-selector";
 
 const MessageInput = ({ chatRoom }) => {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
+  const [isEmojiPickerOpen, setIsEmojiPickerOpen] = useState(false);
 
   const sendMessage = async () => {
     // send message
     const user = await Auth.currentAuthenticatedUser();
-    const newMessage = await DataStore.save(new Message({
-      content: message,
-      userID: user.attributes.sub,
-      chatroomID: chatRoom.id,
-    }))
+    const newMessage = await DataStore.save(
+      new Message({
+        content: message,
+        userID: user.attributes.sub,
+        chatroomID: chatRoom.id,
+      })
+    );
 
     updateLastMessage(newMessage);
 
-    setMessage('');
-  }
+    setMessage("");
+    setIsEmojiPickerOpen(false);
+  };
 
   const updateLastMessage = async (newMessage) => {
-    DataStore.save(ChatRoom.copyOf(chatRoom, updatedChatRoom => {
-      updatedChatRoom.LastMessage = newMessage;
-    }))
-  }
+    DataStore.save(
+      ChatRoom.copyOf(chatRoom, (updatedChatRoom) => {
+        updatedChatRoom.LastMessage = newMessage;
+      })
+    );
+  };
 
   const onPlusClicked = () => {
     console.warn("On plus clicked");
-  }
+  };
 
   const onPress = () => {
     if (message) {
@@ -46,48 +59,87 @@ const MessageInput = ({ chatRoom }) => {
     } else {
       onPlusClicked();
     }
-  }
+  };
 
   return (
-    <KeyboardAvoidingView 
-      style={styles.root} 
+    <KeyboardAvoidingView
+      style={[styles.root, { height: isEmojiPickerOpen ? "50%" : "auto" }]}
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       keyboardVerticalOffset={100}
     >
-      <View style={styles.inputContainer}>
-        <SimpleLineIcons name="emotsmile" size={24} color="#595959" style={styles.icon} />
-        
-        <TextInput 
-          style={styles.input}
-          value={message}
-          onChangeText={setMessage}
-          placeholder="Signal message..."
-        />
-        
-        <Feather name="camera" size={24} color="#595959" style={styles.icon} />
-        <MaterialCommunityIcons name="microphone-outline" size={24} color="#595959" style={styles.icon} />
+      <View style={styles.row}>
+        <View style={styles.inputContainer}>
+          <Pressable
+            onPress={() =>
+              setIsEmojiPickerOpen((currentValue) => !currentValue)
+            }
+          >
+            <SimpleLineIcons
+              name="emotsmile"
+              size={24}
+              color="#595959"
+              style={styles.icon}
+            />
+          </Pressable>
+
+          <TextInput
+            style={styles.input}
+            value={message}
+            onChangeText={setMessage}
+            placeholder="Signal message..."
+          />
+
+          <Feather
+            name="camera"
+            size={24}
+            color="#595959"
+            style={styles.icon}
+          />
+          <MaterialCommunityIcons
+            name="microphone-outline"
+            size={24}
+            color="#595959"
+            style={styles.icon}
+          />
+        </View>
+
+        <Pressable onPress={onPress} style={styles.buttonContainer}>
+          {message ? (
+            <Ionicons name="send" size={18} color="white" />
+          ) : (
+            <AntDesign name="plus" size={24} color="white" />
+          )}
+        </Pressable>
       </View>
-      <Pressable onPress={onPress} style={styles.buttonContainer}>
-        {message ? <Ionicons name="send" size={18} color="white" /> : <AntDesign name="plus" size={24} color="white" />}
-      </Pressable>
+
+      {isEmojiPickerOpen && (
+        <EmojiSelector
+          onEmojiSelected={(emoji) =>
+            setMessage((currentMessage) => currentMessage + emoji)
+          }
+          columns={8}
+        />
+      )}
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   root: {
-    flexDirection: 'row',
     padding: 10,
   },
+  row: {
+    flexDirection: "row",
+  },
   inputContainer: {
-    backgroundColor: '#f2f2f2',
+    backgroundColor: "#f2f2f2",
     flex: 1,
     marginRight: 10,
     borderRadius: 25,
     borderWidth: 1,
-    borderColor: '#dedede',
-    alignItems: 'center',
-    flexDirection: 'row',
+    borderColor: "#dedede",
+    alignItems: "center",
+    flexDirection: "row",
     padding: 5,
   },
   input: {
@@ -100,15 +152,15 @@ const styles = StyleSheet.create({
   buttonContainer: {
     width: 40,
     height: 40,
-    backgroundColor: '#3777f0',
+    backgroundColor: "#3777f0",
     borderRadius: 25,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 35,
-  }
+  },
 });
 
-export default MessageInput
+export default MessageInput;
